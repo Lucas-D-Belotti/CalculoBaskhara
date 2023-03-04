e9c9e0c6bffd480b2df6d8bb0cfaff71f49e781a
Lucas-D-Belotti <99500312+Lucas-D-Belotti@users.noreply.github.com>
2023-03-04 00:17:37 -0300

    Add files via upload

---
+++ baskara_interroçacao/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace baskara_interroçacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resulX1 = new System.Windows.Forms.Label();
+            this.ResulX2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Delta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor de A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor de C:";
+            // 
+            // resulX1
+            // 
+            this.resulX1.AutoSize = true;
+            this.resulX1.Location = new System.Drawing.Point(39, 217);
+            this.resulX1.Name = "resulX1";
+            this.resulX1.Size = new System.Drawing.Size(38, 13);
+            this.resulX1.TabIndex = 2;
+            this.resulX1.Text = "X1 = ?";
+            // 
+            // ResulX2
+            // 
+            this.ResulX2.AutoSize = true;
+            this.ResulX2.Location = new System.Drawing.Point(39, 247);
+            this.ResulX2.Name = "ResulX2";
+            this.ResulX2.Size = new System.Drawing.Size(38, 13);
+            this.ResulX2.TabIndex = 3;
+            this.ResulX2.Text = "X2 = ?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Valor de B:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(104, 22);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 5;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(104, 76);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 6;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(104, 129);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 74);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 40);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Delta
+            // 
+            this.Delta.AutoSize = true;
+            this.Delta.Location = new System.Drawing.Point(39, 182);
+            this.Delta.Name = "Delta";
+            this.Delta.Size = new System.Drawing.Size(34, 13);
+            this.Delta.TabIndex = 10;
+            this.Delta.Text = "▲ = ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 280);
+            this.Controls.Add(this.Delta);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ResulX2);
+            this.Controls.Add(this.resulX1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Calculo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resulX1;
+        private System.Windows.Forms.Label ResulX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Delta;
+    }
+}
+
---
+++ baskara_interroçacao/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baskara_interroçacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, D;
+
+            string s = "▲", l = "2";
+
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            c = Convert.ToDouble(txtC.Text);
+
+            D = Math.Pow(b, Convert.ToInt32(l)) - 4 * a * c;
+
+            Delta.Text = s + " = " + Convert.ToString(D);
+
+            if (D < 0)
+            {
+                resulX1.Text = "Não existe raiz dentro do conjunto dos REAIS";
+                ResulX2.Text = "Não existe raiz dentro do conjunto dos REAIS";
+            }
+            else
+            {
+
+                resulX1.Text = "X1 = " + Convert.ToString((-(b) + Math.Sqrt(D)) / (2 * a));
+                ResulX2.Text = "X2 = " + Convert.ToString((-(b) - Math.Sqrt(D)) / (2 * a));
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+
+            Delta.Text = "▲ = ?";
+
+            resulX1.Text = "X1 = ?";
+            ResulX2.Text = "X2 = ?";
+
+            txtA.Focus();
+        }
+    }
+}
